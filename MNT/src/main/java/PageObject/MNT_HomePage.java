5dcf00fa231c437f39bd273c42967149c4f047ac
litonjabed <jabedliton1@gmail.com>
2019-01-29 12:46:25 -0500

    test

--- MNT/src/main/java/PageObject/MNT_HomePage.java
+++ MNT/src/main/java/PageObject/MNT_HomePage.java
@@ -1,14 +1,21 @@
 package PageObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+
 
 public class MNT_HomePage {
 
@@ -25,11 +32,11 @@ public class MNT_HomePage {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		// launch application
-		driver.get("https://www.weightwatchers.com/us/");
+		driver.get("https://www.mtb.com/home-page");
 		// verify title
 		String title = driver.getTitle();
-		Assert.assertEquals(title, "WW (Weight Watchers): Weight Loss & Wellness Help");
-		System.out.println("Landing Page Title  Verified "+ title);
+		//Assert.assertEquals(title, "WW (Weight Watchers): Weight Loss & Wellness Help");
+		System.out.println("Landing Page Title v :- "+ title);
 	
 		
 //		
