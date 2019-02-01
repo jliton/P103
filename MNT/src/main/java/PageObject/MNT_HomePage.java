cc6a589be7f84ce9da998111020fe5c4d6fab67c
litonjabed <jabedliton1@gmail.com>
2019-01-31 21:27:42 -0500

    hh

--- MNT/src/main/java/PageObject/MNT_HomePage.java
+++ MNT/src/main/java/PageObject/MNT_HomePage.java
@@ -5,12 +5,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
